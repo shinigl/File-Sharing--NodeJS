c32549e53a0d1c173d6ad721afccd438215fc6dc
shinigl <kraniket754@gmail.com>
2025-03-08 21:37:42 +0530

    Updated pass

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/fileshare')
+.connect('mongodb+srv://kraniket754:IZjqAUKDp2xJvJ8i@cluster0.blryc.mongodb.net/')
 .then(()=>console.log('DB connected successfully'))
 .catch((err)=>console.log(`Error connecting DB:`,err))
 
